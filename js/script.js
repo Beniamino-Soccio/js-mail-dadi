b0dd8317f2c835538cf3d2214951929de6c5b36d
Beniamino-Soccio <beniamino43@gmail.com>
2024-08-19 16:48:09 +0200

    random num of dice + winner in console

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 console.log('JS OK');
+// FASE PREPARAZIONE
+let message = "il vincitore è: ";
+//FASE ELABORAZIONE
+// ! genero i due numeri casuali del dado
+
+const numberUser = Math.round(Math.random() * 5) + 1;
+console.log(numberUser);
+
+const numberCpu = Math.round(Math.random() * 5) + 1;
+console.log(numberCpu);
+
+if (numberCpu > numberUser){
+    message += "CPU!"
+} else if (numberCpu < numberUser) {
+    message += "USER!"
+} else {
+    message += "nessuno è un pareggio!"
+}
+
+//FASE OUTPUT
+console.log(message);
